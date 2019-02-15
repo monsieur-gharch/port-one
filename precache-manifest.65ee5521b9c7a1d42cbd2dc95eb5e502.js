1c45bcbe0960bec80b584b2f10a8463e1d67a9c2
Ali mohammadzadeh <ali.carnoat@gmail.com>
2019-02-15 22:24:56 +0330

    Updates

---
+++ precache-manifest.65ee5521b9c7a1d42cbd2dc95eb5e502.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c13dd3c9f9e8190b9c4db521c4f1a0a0",
+    "url": "/port-one/static/media/3.c13dd3c9.mp4"
+  },
+  {
+    "revision": "4f2c1fbbf0d58668174efc16da341b88",
+    "url": "/port-one/static/media/2.4f2c1fbb.jpeg"
+  },
+  {
+    "revision": "dd1b22e7460391dc2cfbc2deac795d03",
+    "url": "/port-one/static/media/1.dd1b22e7.png"
+  },
+  {
+    "revision": "a7a3356e382af8a8c868",
+    "url": "/port-one/static/js/runtime~main.a7a3356e.js"
+  },
+  {
+    "revision": "2ad33a707986f4dc7468",
+    "url": "/port-one/static/js/main.2ad33a70.chunk.js"
+  },
+  {
+    "revision": "775967852f97e5da41ed",
+    "url": "/port-one/static/js/1.77596785.chunk.js"
+  },
+  {
+    "revision": "2ad33a707986f4dc7468",
+    "url": "/port-one/static/css/main.08d33281.chunk.css"
+  },
+  {
+    "revision": "da0c6b5e7ae353ad5638055c7c44d114",
+    "url": "/port-one/index.html"
+  }
+];
